27d137ed2bda8ab570e14e141661b271cf6ccfd6
danie <danie@LAPTOP-KCRC8BJ8>
2022-09-21 09:46:05 +0200

    Add project files.

---
+++ Campus220921/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Campus220921
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Anas";      ///
+            dynamic name2 = "Annannas";
+            Console.WriteLine(name, name2);
+            var numm = 10;
+            dynamic numdym = 3939;  ///
+            Console.WriteLine(+ numm + numdym);
+
+
+        }
+    }
+}
